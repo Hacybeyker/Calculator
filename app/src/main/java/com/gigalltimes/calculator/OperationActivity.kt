248e20553cfe92da23e5d40d1c601ffdaa5115c7
Carlos Osorio Perez <hacybeyker@gmailcom>
2020-02-06 00:33:21 -0500

    project init

---
+++ app/src/main/java/com/gigalltimes/calculator/OperationActivity.kt
@@ -0,0 +1,12 @@
+package com.gigalltimes.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OperationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_operation)
+    }
+}
---
+++ app/src/main/java/com/gigalltimes/calculator/Utils.kt
@@ -0,0 +1,6 @@
+package com.gigalltimes.calculator
+
+fun suma(numberOne: Double, numberTwo: Double): Double = numberOne.plus(numberTwo)
+fun resta(numberOne: Double, numberTwo: Double): Double = numberOne.minus(numberTwo)
+fun multiplicacion(numberOne: Double, numberTwo: Double): Double = numberOne.times(numberTwo)
+fun division(numberOne: Double, numberTwo: Double): Double = numberOne.div(numberTwo)
